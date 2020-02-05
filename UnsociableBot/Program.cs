75f496fb96b3d83cf3a572a118366525ba47e8bc
Moretti <moretti@ls-v.com>
2020-02-05 11:34:41 +0000

    Start of TwitchBot intergration

---
+++ UnsociableBot/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Timer = System.Timers.Timer;
+
+namespace UnsociableBot
+{
+    internal class Program
+    {
+        private static bool _quitFlag = false;
+
+        private static void Main(string[] args)
+        {
+            Console.Title = $"UnsociableBot";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                _quitFlag = true;
+            };
+
+            Settings.Default.Reload();
+
+            Debug.WriteLine($"Upgrade Required: {Settings.Default.UpgradeRequired}.");
+
+            if (Settings.Default.UpgradeRequired)
+            {
+                Settings.Default.Upgrade();
+                Settings.Default.UpgradeRequired = false;
+                Settings.Default.Save();
+            }
+
+            Settings.Default.Reload();
+            Console.WriteLine("Starting the UnsociableBot!");
+
+            if (string.IsNullOrEmpty(Settings.Default.TwitchUsername))
+            {
+                Console.WriteLine($"What is your twitch username?");
+                string twitchUsername = Console.ReadLine();
+
+                bool userConfirm = false;
+
+                Console.WriteLine($"Confirm your username is: {twitchUsername}. Y/N");
+                string twitchUsernameConfirm = Console.ReadLine();
+                if (twitchUsernameConfirm.ToLower() == "y")
+                {
+                    userConfirm = true;
+                }
+
+                while (!userConfirm)
+                {
+                    Console.WriteLine($"What is your twitch username?");
+                    twitchUsername = Console.ReadLine();
+                    Console.WriteLine($"Confirm your username is: {twitchUsername}. Y/N");
+                    twitchUsernameConfirm = Console.ReadLine();
+                    if (twitchUsernameConfirm.ToLower() == "y")
+                    {
+                        userConfirm = true;
+                    }
+                }
+
+                Settings.Default.TwitchUsername = twitchUsername;
+                Settings.Default.Save();
+            }
+
+            Settings.Default.Reload();
+
+            Console.WriteLine("Settings have been reloaded!");
+
+            TwitchBot bot = new TwitchBot();
+
+            while (!_quitFlag)
+            {
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ UnsociableBot/TwitchBot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Timers;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace UnsociableBot
+{
+    public class TwitchBot
+    {
+        private TwitchClient client;
+
+        public TwitchBot()
+        {
+            bool connected = false;
+
+            Console.WriteLine($"Connecting to Twitch..");
+
+            ConnectionCredentials credentials = new ConnectionCredentials(Settings.Default.TwitchUsername, Settings.Default.TwitchClientToken);
+
+            ClientOptions clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, Settings.Default.TwitchUsername);
+
+            client.OnLog += Client_OnLog;
+
+            client.Connect();
+
+            Timer timer = new Timer(500) { AutoReset = true };
+            timer.Start();
+
+            timer.Elapsed += (sender, args) =>
+            {
+                Console.WriteLine("..");
+            };
+
+            while (!connected)
+            {
+                connected = client.IsConnected;
+            }
+
+            timer.Dispose();
+
+            Console.WriteLine($"Connected to Twitch.");
+
+            Console.ReadLine();
+        }
+
+        private void Client_OnLog(object sender, TwitchLib.Client.Events.OnLogArgs e)
+        {
+            Console.WriteLine($"{e.DateTime.ToString(CultureInfo.CurrentCulture)}: {e.BotUsername} - {e.Data}");
+        }
+    }
+}
